e17731e9e9a5a2c95ee9cd8d1581e9623b28de01
= <=>
2025-03-19 01:59:33 +0300

    project finish

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+class Program
+{
+    static void Main()
+    {
+        int randomNum = (new Random()).Next(1, 100);
+        int guess = 5;
+        int number;
+        int tour = 0;
+
+        while (guess > 0)
+        {
+            tour++;
+            if (tour == 1)
+            {
+                Console.WriteLine("Welcome to the guessing game!");
+            }
+            Console.WriteLine("Enter your guess: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            guess--;
+            if (number == randomNum)
+            {
+                Console.WriteLine($"You got it! {randomNum} , you guessed in {tour} rounds");
+                break;
+            }
+            else
+            {
+                if (number < randomNum)
+                {
+                    Console.WriteLine("Too low");
+                }
+                else if (number > randomNum)
+                {
+                    Console.WriteLine("Too high");
+                }
+            }
+            Console.WriteLine("You have " + guess + " guesses left");
+        }
+
+    }
+}
